448464e4c3efbf51bb6021ab427f4b32da7ef1be
wooho <woohobi@gmail.com>
2022-08-08 14:26:22 +0900

    Add Default theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useState} from "react";
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
+import Layout from "../components/layout/Layout";
+import {ThemeProvider} from "styled-components";
+import {theme} from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp
---
+++ styles/theme.ts
@@ -0,0 +1,7 @@
+interface DefaultTheme {
+  borderGrey: string,
+}
+
+export const theme: DefaultTheme = {
+  borderGrey: "#E9ECEF"
+};
