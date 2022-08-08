a7460cfd2f1792e214d6e677657f5e8f56e95ba3
wooho <woohobi@gmail.com>
2022-08-08 15:28:26 +0900

    Feat theme infinite fetch

---
+++ utils/posts.ts
@@ -0,0 +1,8 @@
+import {httpClient} from "./httpClient";
+
+const OFFSET = 0;
+
+export const fetchPostsInfinite = async ({pageParam = OFFSET}) => {
+  const response = await httpClient.get(`/api/posts?page=${pageParam}&size=10`)
+  return await response.data;
+};
