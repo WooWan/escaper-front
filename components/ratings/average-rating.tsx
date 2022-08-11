a6654018da009b818e48e575bf27ee73e69473b4
wooho <woohobi@gmail.com>
2022-08-11 23:43:28 +0900

    Add 유저 평균 평점

--- components/ratings/average-rating.tsx
+++ components/ratings/average-rating.tsx
@@ -1,20 +1,21 @@
 import React, {useState} from 'react';
-import { Rating } from 'react-simple-star-rating';
 import {RatingTitle, RatingInfo, Score, ScoreInfo, GeneralRating} from "../../styles/components";
 import styled from "styled-components";
+import {Rating} from "../rating-bar/rating";
 
 const UserRatingBox= styled(GeneralRating)`
 
 `
+
 function AverageRating() {
-  const [rating, setRating] = useState(0)
+  const [rating] = useState(0);
   return (
     <UserRatingBox>
       <RatingTitle>평균 별점</RatingTitle>
       <RatingInfo>
         <Score>3.0</Score>
         <ScoreInfo>778개의 별점</ScoreInfo>
-        <Rating ratingValue={rating} readonly/>
+        <Rating ratingValue={rating} readonly transition={true}/>
       </RatingInfo>
     </UserRatingBox>
   );
