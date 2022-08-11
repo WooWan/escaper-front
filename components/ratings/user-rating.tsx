0cf05ed8a9c6dc6812aa48ccd0ec43c982e4cf36
wooho <woohobi@gmail.com>
2022-08-11 23:43:37 +0900

    Add 유저 개인 평점

--- components/ratings/user-rating.tsx
+++ components/ratings/user-rating.tsx
@@ -1,20 +1,23 @@
 import {useState} from 'react';
-import {Rating} from 'react-simple-star-rating';
 import styled from "styled-components";
-import {GeneralRating, RatingInfo, RatingTitle, Score, ScoreInfo} from "../../styles/components";
+import {GeneralRating, RatingInfo, RatingTitle, Score} from "../../styles/components";
+import {Rating} from "../rating-bar/rating";
 
 const UserRatingBox = styled(GeneralRating)`
 `
 
 function UserRating() {
   const [rating, setRating] = useState(0)
+  const onHandleRating = (event:number)=>{
+    setRating(event)
+  }
+  const toolTipArray = ["별점을 남겨주세요","싫어요", "재미 없어요", "별로에요", "많이 아쉬워요", "흙길", "풀길", "풀꽃길", "꽃길", "꽃밭길", "인생 테마에요!"];
   return (
     <UserRatingBox>
       <RatingTitle>내 별점</RatingTitle>
       <RatingInfo>
-        <Score>3.0</Score>
-        <ScoreInfo>별점을 남겨주세요</ScoreInfo>
-        <Rating ratingValue={rating} allowHalfIcon/>
+        <Score>{rating}</Score>
+        <Rating ratingValue={rating} allowHalfIcon transition={true} tooltipArray={toolTipArray} showTooltip  tooltipDefaultText={"별점을 남겨주세요"} onClick={onHandleRating}/>
       </RatingInfo>
     </UserRatingBox>
   );
