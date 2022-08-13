0450297d92d66c61bfcdde78aca18be99857808e
wooho <woohobi@gmail.com>
2022-08-13 12:55:54 +0900

    Add review register component

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,4 +1,5 @@
 import {httpClient} from "./httpClient";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 
 export async function fetchThemeById(id: string) {
   const response = await httpClient.get(`/api/theme/${id}`);
@@ -18,4 +19,25 @@ export async function fetchThemeByGenre(genre: string) {
 export async function fetchThemeTypes() {
   const response = await httpClient.get(`/api/themes/type`)
   return await response.data;
+}
+interface IReview {
+  themeId: string | string[] | undefined
+  review: string
+}
+
+export const fetchReview = async (themeId: string | string [] | undefined) => {
+  const response = await httpClient.get(`/api/review/${Number(themeId)}`)
+  return response.data;
+};
+export function addReview({themeId, review}: IReview) {
+  return httpClient.post(`/api/review/${themeId}`, {themeId, content: review});
+}
+
+export function useAddReview(themeId: string | string[] | undefined) {
+  const queryClient = useQueryClient();
+  return useMutation(addReview, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['review', themeId])
+    }
+  });
 }
