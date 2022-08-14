aebd2744fdd273318317e6093265141173ceab6b
wooho <woohobi@gmail.com>
2022-08-14 14:33:17 +0900

    style 색상 변경

--- components/comment/comment.tsx
+++ components/comment/comment.tsx
@@ -1,13 +1,14 @@
-import {ICommentResponse} from "../../types";
+import { ICommentResponse } from "../../types";
 import styled from "styled-components";
 
 const CommentBox = styled.div`
   padding-bottom: 1.5rem;
   padding-top: 1.5rem;
   border-bottom: 1px solid;
-  border-bottom-color: ${props => props.theme.borderGrey};
-`
-function Comment({content}: ICommentResponse) {
+  border-bottom-color: ${(props) => props.theme.gray};
+`;
+
+function Comment({ content }: ICommentResponse) {
   return (
     <CommentBox>
       <p>{content}</p>
