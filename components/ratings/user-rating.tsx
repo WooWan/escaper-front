c4568e36aed5b26a12f3ef9e1f2e86b523d65837
wooho <woohobi@gmail.com>
2022-08-18 22:59:24 +0900

    Add user rating

--- components/ratings/user-rating.tsx
+++ components/ratings/user-rating.tsx
@@ -1,23 +1,57 @@
-import {useState} from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import styled from "styled-components";
-import {GeneralRating, RatingInfo, RatingTitle, Score} from "../../styles/components";
-import {Rating} from "../core/rating-bar/rating";
+import {
+  GeneralRating,
+  RatingInfo,
+  RatingTitle,
+  Score,
+} from "../../styles/components";
+import { rateTheme } from "../../utils/rating";
+import { Rating } from "../core/rating-bar/rating";
 
-const UserRatingBox = styled(GeneralRating)`
-`
+const UserRatingBox = styled(GeneralRating)``;
 
 function UserRating() {
-  const [rating, setRating] = useState(0)
-  const onHandleRating = (event:number)=>{
-    setRating(event)
-  }
-  const toolTipArray = ["별점을 남겨주세요","싫어요", "재미 없어요", "별로에요", "많이 아쉬워요", "흙길", "풀길", "풀꽃길", "꽃길", "꽃밭길", "인생 테마에요!"];
+  const router = useRouter();
+  const [rating, setRating] = useState(0);
+  const themeId = router.query.id;
+
+  const { mutate: handleRate } = useMutation(rateTheme);
+
+  const onHandleRating = (rate: number) => {
+    setRating(rate);
+    handleRate({ themeId, rate });
+  };
+  const toolTipArray = [
+    "별점을 남겨주세요",
+    "싫어요",
+    "재미 없어요",
+    "별로에요",
+    "많이 아쉬워요",
+    "흙길",
+    "풀길",
+    "풀꽃길",
+    "꽃길",
+    "꽃밭길",
+    "인생 테마에요!",
+  ];
+
   return (
     <UserRatingBox>
       <RatingTitle>내 별점</RatingTitle>
       <RatingInfo>
         <Score>{rating}</Score>
-        <Rating ratingValue={rating} allowHalfIcon transition={true} tooltipArray={toolTipArray} showTooltip  tooltipDefaultText={"별점을 남겨주세요"} onClick={onHandleRating}/>
+        <Rating
+          ratingValue={rating}
+          allowHalfIcon
+          transition={true}
+          tooltipArray={toolTipArray}
+          showTooltip
+          tooltipDefaultText={"별점을 남겨주세요"}
+          onClick={onHandleRating}
+        />
       </RatingInfo>
     </UserRatingBox>
   );
