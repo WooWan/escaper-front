62f4769c97a14a39189dee116488f416d3235448
wooho <woohobi@gmail.com>
2022-08-20 18:10:23 +0900

    Feat 방탈출 테마 리뷰

--- pages/theme/[id].tsx
+++ pages/theme/[id].tsx
@@ -1,46 +1,63 @@
-import {GetStaticPropsContext} from "next";
-import {ITheme} from "../../types";
-import {httpClient} from "../../utils/httpClient";
-import {fetchThemeById} from "../../utils/theme";
+import { GetStaticPropsContext } from "next";
+import { IReview, ITheme } from "../../interfaces";
+import { httpClient } from "../../utils/httpClient";
+import { fetchReview, fetchThemeById } from "../../utils/theme";
 import styled from "styled-components";
 import ThemeInfo from "../../components/theme/ThemeInfo";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import ReviewRegister from "../../components/review/review-register";
+import Reviews from "../../components/review/Reviews";
 
-interface IProps {
-  theme: ITheme
-}
-
-const ThemeWrapper= styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   max-width: 920px;
   margin: 0 auto;
   height: 100%;
-`
+`;
 
+const ReviewWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 
+interface IProps {
+  theme: ITheme;
+}
 
-function ThemePage({theme}:IProps) {
+function ThemePage({ theme }: IProps) {
+  const router = useRouter();
+  const themeId = router.query.id;
+
+  const { data: reviews } = useQuery<IReview[]>(["review", themeId], () =>
+    fetchReview(themeId)
+  );
   return (
-    <ThemeWrapper>
-     <ThemeInfo theme={theme}/>
-    </ThemeWrapper>
+    <Container>
+      <ThemeInfo theme={theme} />
+      <ReviewWrapper>
+        <ReviewRegister />
+        <Reviews reviews={reviews} />
+      </ReviewWrapper>
+    </Container>
   );
 }
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-  const id = context.params?.id ?? "";
-  const theme = await fetchThemeById(id.toString());
-
-  return {props: {theme}, revalidate: 3600};
+  const id = context.params?.id;
+  const theme = await fetchThemeById(id);
+  console.log(theme);
+  return { props: { theme }, revalidate: 3600 };
 }
 
 export async function getStaticPaths() {
   const response = await httpClient.get<ITheme[]>("/api/themes");
   const data = await response.data;
-  const paths = data?.map(theme => ({ params: {id: theme.id.toString()}}) )
+  const paths = data?.map((theme) => ({ params: { id: theme.id.toString() } }));
 
-  return {paths, fallback: "blocking"}
+  return { paths, fallback: "blocking" };
 }
 
 export default ThemePage;
