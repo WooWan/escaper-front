837edbf79b8dda189c0e82983c5592bc9dda8b0c
wooho <woohobi@gmail.com>
2022-08-20 18:13:48 +0900

    Fix 세션스토리지로 변경

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,8 +1,14 @@
 export const setStorageItem = (key: string, value: string) => {
-  const storage = window.localStorage;
+  const storage = window.sessionStorage;
   try {
     storage.setItem(key, value);
   } catch (e) {
     console.error(e);
   }
+};
+
+export const getStorageItem = (key: string) => {
+  const storage = window.sessionStorage;
+  const storedValue = storage.getItem(key);
+  return storedValue ? storedValue : "default";
 };
