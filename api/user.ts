4a31f52de247de522db27c2e0dd46ed0ce7c3dde
wooho <woohobi@gmail.com>
2022-08-20 18:14:26 +0900

    remove 파일 변경

--- api/user.ts
+++
@@ -1,5 +0,0 @@
-import { httpClient } from "./../utils/httpClient";
-export const login = async (id: number) => {
-  const { data } = await httpClient.post("/api/login", { id: id });
-  return data;
-};
