f44a584d4ba6ce4251de762fdb4f4cfbcf08315b
wooho <woohobi@gmail.com>
2022-08-21 19:42:26 +0900

    Fix window ssr 대응

--- utils/hooks/useAxiosInterceptor.ts
+++ utils/hooks/useAxiosInterceptor.ts
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Cookies } from "react-cookie";
 import { httpClient } from "./../httpClient";
 import { getStorageItem } from "./../storage";
@@ -5,14 +6,16 @@ import { getStorageItem } from "./../storage";
 export const useAxiosInterceptor = () => {
   const cookies = new Cookies();
 
-  httpClient.interceptors.request.use((config) => {
-    const accessToken = getStorageItem("token");
-    const refreshToken = cookies.get("refresh-token");
+  useEffect(() => {
+    httpClient.interceptors.request.use((config) => {
+      const accessToken = getStorageItem("token");
+      const refreshToken = cookies.get("refresh-token");
 
-    if (config.headers && accessToken) {
-      config.headers["Authorization"] = `Bearer ${accessToken}`;
-      config.headers["refreshToken"] = refreshToken;
-    }
-    return config;
+      if (config.headers && accessToken) {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+        config.headers["refreshToken"] = refreshToken;
+      }
+      return config;
+    });
   });
 };
