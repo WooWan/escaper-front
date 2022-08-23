252d1cfe01eaf64aade683d651c48525abc8dea9
wooho <woohobi@gmail.com>
2022-08-23 18:09:58 +0900

    Feat 방탈출 테마 평점

--- utils/rating.ts
+++ utils/rating.ts
@@ -1,8 +1,25 @@
-import { IRate } from "../interfaces";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { IRatingRequest } from "../interfaces";
 import { httpClient } from "./httpClient";
 
-export const rateTheme = async ({ themeId, memberId, rating }: IRate) => {
+export const rateTheme = async ({
+  themeId,
+  memberId,
+  rating,
+}: IRatingRequest) => {
   return await httpClient.post(`/api/rate/${themeId}/member/${memberId}`, {
     rating,
+  });
+};
+
+export const useRateTheme = (
+  memberId: number | undefined,
+  themeId: string | string[] | undefined
+) => {
+  const queryClient = useQueryClient();
+  return useMutation(rateTheme, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["rating", memberId, themeId]);
+    },
   });
 };
