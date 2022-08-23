1e2546e302c21d403e72354a82c2f3750be4421e
wooho <woohobi@gmail.com>
2022-08-23 17:58:52 +0900

    Add Login 요청 모달

---
+++ components/modal/login-error-modal/LoginErrorModal.style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2.75rem 1.5rem 1.5rem 1.5rem;
+  width: 100%;
+  height: 100%;
+`;
+export const Footer = styled.footer`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`;
+export const ButtonWrapper = styled.div`
+  display: flex;
+  column-gap: 0.5rem;
+`;
+export const Content = styled.span`
+  font-size: 1.3rem;
+  font-weight: bolder;
+`;
---
+++ components/modal/login-error-modal/LoginErrorModal.tsx
@@ -0,0 +1,48 @@
+import { useDispatch } from "react-redux";
+import { closeModal, openModal } from "../../../store/slices/Modal";
+import TextButton from "../../core/button/text-button/TextButton";
+import Modal from "../basic/Modal";
+import {
+  ButtonWrapper,
+  Content,
+  Footer,
+  Wrapper,
+} from "./LoginErrorModal.style";
+
+function LoginErrorModal() {
+  const dispatch = useDispatch();
+  const handleModalOpen = () => {
+    dispatch(closeModal());
+    dispatch(
+      openModal({
+        modalType: "LoginModal",
+        content: "login",
+        isOpen: true,
+      })
+    );
+  };
+  return (
+    <Modal format={"horizontal"}>
+      <Wrapper>
+        <main>
+          <Content>로그인 후 이용해주세요</Content>
+        </main>
+        <Footer>
+          <ButtonWrapper>
+            <TextButton
+              buttonType="destructive"
+              onClick={() => dispatch(closeModal())}
+            >
+              취소
+            </TextButton>
+            <TextButton buttonType="primary" onClick={handleModalOpen}>
+              로그인
+            </TextButton>
+          </ButtonWrapper>
+        </Footer>
+      </Wrapper>
+    </Modal>
+  );
+}
+
+export default LoginErrorModal;
