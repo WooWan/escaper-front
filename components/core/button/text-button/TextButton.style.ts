4ed8cc8e632567ffb99951291a82178c1a2a2adb
wooho <woohobi@gmail.com>
2022-08-23 17:58:17 +0900

    Design 공통 버튼 색상 커스터마이징

--- components/core/button/text-button/TextButton.style.ts
+++ components/core/button/text-button/TextButton.style.ts
@@ -1,12 +1,28 @@
-import styled from "styled-components";
+import styled, { DefaultTheme } from "styled-components";
+import { ButtonType } from "./TextButton";
 
-export const Button = styled.button`
-  background-color: ${(props) => props.theme.primaryColor};
+const handleButtonColor = (theme: DefaultTheme, buttonType?: ButtonType) => {
+  switch (buttonType) {
+    case "primary":
+      console.log(theme.primaryColor);
+      return theme.primaryColor;
+    case "destructive":
+      console.log(theme.destructiveColor);
+      return theme.destructiveColor;
+    default:
+      return theme.secondaryColor;
+  }
+};
+
+export const Button = styled.button<{ buttonType?: ButtonType }>`
   color: ${(props) => props.theme.buttonText};
-  border-radius: 15%;
+  border-radius: 0.5rem;
   width: 4rem;
   height: 2rem;
   font-size: 0.875rem;
   font-weight: 500;
   text-align: center;
+  cursor: pointer;
+  background-color: ${({ buttonType, theme }) =>
+    handleButtonColor(theme, buttonType)};
 `;
--- components/core/button/text-button/TextButton.tsx
+++ components/core/button/text-button/TextButton.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from "react";
 import { Button } from "./TextButton.style";
 
+export type ButtonType = "primary" | "destructive" | "basic";
 interface Props {
-  onClick: () => void;
+  onClick?: () => void;
   children?: string | ReactNode;
+  buttonType?: ButtonType;
 }
 
-function TextButton({ onClick, children }: Props) {
-  return <Button onClick={onClick}>{children}</Button>;
+function TextButton({ onClick, children, buttonType }: Props) {
+  return (
+    <Button onClick={onClick} buttonType={buttonType}>
+      {children}
+    </Button>
+  );
 }
 
 export default TextButton;
