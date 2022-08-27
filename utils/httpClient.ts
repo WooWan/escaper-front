4059abc4c0426756deb465980dcdc9c633ced534
wooho <woohobi@gmail.com>
2022-08-27 22:23:16 +0900

    Chore api url 환경변수화

--- utils/httpClient.ts
+++ utils/httpClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const httpClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: API_URL,
   headers: {
     "Content-Type": `application/json;charset=UTF-8`,
     "Access-Control-Allow-Origin": "*",
