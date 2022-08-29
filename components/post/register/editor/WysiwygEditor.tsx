95d3379025a598ec4c4a43edf1431cb115028f07
wooho <woohobi@gmail.com>
2022-08-29 23:44:21 +0900

    Add wysiwyg editor

---
+++ components/post/register/editor/WysiwygEditor.tsx
@@ -0,0 +1,54 @@
+import dynamic from "next/dynamic";
+import * as React from "react";
+import { Editor as EditorType, EditorProps } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { TuiEditorWithForwardedProps } from "./TuiEditorWrapper";
+
+interface EditorPropsWithHandlers extends EditorProps {
+  onChange?(value: string): void;
+}
+
+const Editor = dynamic<TuiEditorWithForwardedProps>(
+  () => import("./TuiEditorWrapper"),
+  { ssr: false }
+);
+const EditorWithForwardedRef = React.forwardRef<
+  EditorType | undefined,
+  EditorPropsWithHandlers
+>((props, ref) => (
+  <Editor {...props} forwardedRef={ref as React.MutableRefObject<EditorType>} />
+));
+EditorWithForwardedRef.displayName = "EditorWithForwardedRef";
+
+interface IWysiwygEditor extends EditorProps {
+  onChange(value: string): void;
+  valueType?: "markdown" | "html";
+}
+
+function WysiwygEditor(props: IWysiwygEditor) {
+  const { height, initialEditType, useCommandShortcut, onChange } = props;
+  const editorRef = React.useRef<EditorType>();
+  const placeHolder = "내용을 입력해주세요.";
+  const handleChange = React.useCallback(() => {
+    if (!editorRef.current) {
+      return;
+    }
+    const instance = editorRef.current.getInstance();
+    onChange(instance.getMarkdown());
+  }, [props, editorRef, onChange]);
+
+  return (
+    <EditorWithForwardedRef
+      placeholder={placeHolder}
+      previewStyle="vertical"
+      height={height || "600px"}
+      initialValue=" "
+      initialEditType={"wysiwyg" || initialEditType}
+      useCommandShortcut={useCommandShortcut || true}
+      ref={editorRef}
+      onChange={handleChange}
+    />
+  );
+}
+
+export default WysiwygEditor;
