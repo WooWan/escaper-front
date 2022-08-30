6ce3b7e5f4d2209df2dba64c4d0f41c04e8c942d
wooho <woohobi@gmail.com>
2022-08-30 13:15:05 +0900

    Design counter

--- components/post/register/counter/Counter.tsx
+++ components/post/register/counter/Counter.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
 `;
 interface ICounter {
   participation: number;
@@ -9,17 +10,34 @@ interface ICounter {
   decrementCount: () => void;
 }
 
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 1rem;
+  height: 1rem;
+  background-color: transparent;
+  border: 1px solid gray;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1rem;
+  column-gap: 0.2rem;
+  padding-left: 0.5rem;
+`;
 function Counter({ incrementCount, decrementCount, participation }: ICounter) {
   return (
     <Container>
       <span>모집인원</span>
-      <button type="button" onClick={decrementCount}>
-        -
-      </button>
-      <span>{participation}</span>
-      <button type="button" onClick={incrementCount}>
-        +
-      </button>
+      <ButtonWrapper>
+        <Button onClick={decrementCount}>-</Button>
+        <span>{participation}</span>
+        <Button onClick={incrementCount}>+</Button>
+      </ButtonWrapper>
     </Container>
   );
 }
