85b7d06dc7622446d8c106a0ff58d3cd81902adc
wooho <woohobi@gmail.com>
2022-08-31 14:44:54 +0900

    Design 글 쓰기 레이아웃

--- components/post/register/form/RegisterForm.style.ts
+++ components/post/register/form/RegisterForm.style.ts
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import TextButton from "../../../core/button/text-button/TextButton";
 
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 1040px;
+  padding: 4rem 2rem;
+`;
+
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
   position: relative;
-  width: 600px;
+
   margin: 0 auto;
   gap: 16px;
 `;
@@ -25,4 +31,10 @@ export const Input = styled.input`
   &:focus {
     outline: none;
   }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: end;
+  column-gap: 0.5rem;
 `;
