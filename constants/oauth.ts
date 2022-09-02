080dc40a72d5ba4a3dcf9fc45fd6334ca6e72344
wooho <woohobi@gmail.com>
2022-09-02 22:00:03 +0900

    Chore redirect url 변경

--- constants/oauth.ts
+++ constants/oauth.ts
@@ -1,2 +1,2 @@
-export const googleUrl = `http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000/oauth2/redirect`;
-export const kakaoUrl = `http://localhost:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/oauth2/redirect`;
+export const googleUrl = `http://localhost:8080/oauth2/authorization/google?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}`;
+export const kakaoUrl = `http://localhost:8080/oauth2/authorization/kakao?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}`;
