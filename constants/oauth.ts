1bc9ddf21f2a4297f1d05c02c25d2494d40f8110
wooho <woohobi@gmail.com>
2022-09-06 13:59:30 +0900

    Chore redirect url 환경변수 설정

--- constants/oauth.ts
+++ constants/oauth.ts
@@ -1,2 +1,2 @@
-export const googleUrl = `http://localhost:8080/oauth2/authorization/google?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}`;
-export const kakaoUrl = `http://localhost:8080/oauth2/authorization/kakao?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}`;
+export const googleUrl = `http://localhost:8080/oauth2/authorization/google?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}&signup=${process.env.NEXT_PUBLIC_SIGNUP}`;
+export const kakaoUrl = `http://localhost:8080/oauth2/authorization/kakao?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}&signup=${process.env.NEXT_PUBLIC_SIGNUP}`;
