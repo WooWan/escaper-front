5f81c72298919f2ea0db4a1d2d7b027538eecb70
wooho <woohobi@gmail.com>
2022-09-12 12:45:08 +0900

    Update icon button 타입 추가

--- components/core/button/icon-button/IconButton.tsx
+++ components/core/button/icon-button/IconButton.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, SVGProps } from "react";
+import { ButtonType } from "../text-button/TextButton";
 import { IconButtonWrapper } from "./IconButton.style";
 
 interface Props {
@@ -6,11 +7,12 @@ interface Props {
   children?: string | ReactNode;
   title?: string;
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  buttonType?: ButtonType;
 }
 
-function IconButton({ onClick, children, title, Icon }: Props) {
+function IconButton({ onClick, children, title, Icon, buttonType }: Props) {
   return (
-    <IconButtonWrapper>
+    <IconButtonWrapper buttonType={buttonType}>
       <Icon />
       <p>{title}</p>
       <p>{children}</p>
