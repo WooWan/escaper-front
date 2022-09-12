be13b0f18981477d32330b2d44831d5886b2fe01
wooho <woohobi@gmail.com>
2022-09-12 12:47:01 +0900

    Design 별점 섹션

--- components/ratings/rating-section.tsx
+++ components/ratings/rating-section.tsx
@@ -2,21 +2,25 @@ import AverageRating from "./average-rating";
 import UserRating from "./user-rating";
 import styled from "styled-components";
 
-const RatingWrapper = styled.ul`
+const Container = styled.ul`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  margin-top: 1rem;
+  max-height: 140px;
+  min-height: 100px;
 `;
 interface IProps {
-  themeRating: number;
   memberRating: number | undefined;
+  count: number;
+  averageRating: number;
 }
-function RatingSection({ themeRating, memberRating }: IProps) {
+function RatingSection({ memberRating, count, averageRating }: IProps) {
   return (
-    <RatingWrapper>
+    <Container>
       <UserRating rating={memberRating} />
-      <AverageRating rating={themeRating} />
-    </RatingWrapper>
+      <AverageRating rating={averageRating} count={count} />
+    </Container>
   );
 }
 
