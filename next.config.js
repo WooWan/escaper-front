96631d00b7b59894289d7de8ed7e00d790eabba1
wooho <woohobi@gmail.com>
2022-09-21 14:00:26 +0900

    Chore bundle analyzer 추가

--- next.config.js
+++ next.config.js
@@ -1,16 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
+  compress: true,
   compiler: {
     styledComponents: {
       ssr: true,
       displayName: true,
     },
   },
+  webpack(config) {
+    console.log(config);
+    let prod = process.env.NODE_ENV === "production";
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
+    };
+  },
   images: {
     domains: ["escapertest.s3.ap-northeast-2.amazonaws.com"],
   },
-};
+});
 
 module.exports = nextConfig;
