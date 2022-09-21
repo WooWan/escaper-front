08fa4b51ace959a05a0236ea5c160743924fb92f
wooho <woohobi@gmail.com>
2022-09-21 19:18:55 +0900

    Update ssg 지원

--- pages/theme/index.tsx
+++ pages/theme/index.tsx
@@ -1,27 +1,19 @@
 import Sliders from "../../components/theme/slider/SlidersWrapper";
 import { IThemesType } from "../../interfaces";
-import {
-  dehydrate,
-  QueryClient,
-  useQuery,
-  useQueryClient,
-} from "@tanstack/react-query";
-import { fetchThemeTypes } from "../../api/theme";
-import { Suspense } from "react";
+import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+import { fetchPopularTheme, fetchThemeTypes } from "../../api/theme";
 
 function Theme() {
   const { data } = useQuery<IThemesType[]>(["themeTypes"], fetchThemeTypes);
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Sliders data={data} />
-    </Suspense>
-  );
+  return <Sliders data={data} />;
 }
 
 export async function getStaticProps() {
   const queryClient = new QueryClient();
-  await queryClient.fetchQuery(["themeTypes"], fetchThemeTypes);
+
+  await queryClient.fetchQuery(["popular"], fetchPopularTheme);
+  // await queryClient.fetchQuery(["themeTypes"], fetchThemeTypes);
 
   return { props: { dehydratedState: dehydrate(queryClient) } };
 }
