ff3f39fd5582ff2a53919cc8b69c9b4380583b3a
wooho <woohobi@gmail.com>
2022-10-15 22:01:40 +0900

    Rename fetch member -> login member

--- api/member.ts
+++ api/member.ts
@@ -1,8 +1,8 @@
-import { httpClient } from "./../service/httpClient";
+import { httpClient } from "../service/httpClient";
 import { IMember } from "../interfaces/member";
 import { ApiResponse } from "../interfaces/apiResponse";
 
-export const fetchMember = async () => {
+export const loginMember = async () => {
   const response = await httpClient.get<ApiResponse<IMember>>("/api/member");
   return response.data;
 };
