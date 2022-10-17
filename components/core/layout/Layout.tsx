bea4ff5969d7ac617bc8304e0135db72b06bb928
wooho <woohobi@gmail.com>
2022-10-17 19:27:48 +0900

    Fix css global style by next.js

--- components/core/layout/Layout.tsx
+++ components/core/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 import { ReactNode, useEffect } from "react";
 import NavigationHeader from "../../containers/navigation-header/NavigationHeader";
 import { useAxiosInterceptor } from "../../../utils/hooks/useAxiosInterceptor";
@@ -16,66 +16,6 @@ interface LayoutProps {
 export const Container = styled.div`
   max-width: 1020px;
   margin: 0 auto;
-`;
-
-const GlobalStyle = createGlobalStyle`
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, menu, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  main, menu, nav, output, ruby, section, summary,
-  time, mark, audio, video, button {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font: inherit;
-    vertical-align: baseline;
-    font-family: 'Noto Sans KR', sans-serif;
-  }
-  ul, li {
-    list-style: none;
-  }
-  button {
-    background-color: transparent;
-  }
-
-
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, main, menu, nav, section {
-    display: block;
-  }
-
-  /* HTML5 hidden-attribute fix for newer browsers */
-  *[hidden] {
-    display: none;
-  }
-  body {
-    line-height: 1;
-    font-family: 'Open Sans', sans-serif;
-    font-weight: 300;
-  }
-  menu, ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
 `;
 
 function Layout({ children }: LayoutProps) {
@@ -121,7 +61,6 @@ function Layout({ children }: LayoutProps) {
   return (
     <Container>
       <ModalManager />
-      <GlobalStyle />
       <NavigationHeader />
       <div>{children}</div>
     </Container>
