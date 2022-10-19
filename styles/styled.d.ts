d14d15386fd712c566d47124d1afd2f590ccd188
wooho <woohobi@gmail.com>
2022-10-19 13:24:56 +0900

    Feat dark mode

--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -2,21 +2,40 @@ import "styled-components";
 
 // and extend them!
 declare module "styled-components" {
-  export interface DefaultTheme {
-    textColor: string;
-    bgColor: string;
-    primaryColor: string;
-    secondaryColor: string;
-    buttonText: string;
-    destructiveColor: string;
-    border: string;
-    gray: string;
-    gray_normal: string;
-    gray_lighter: string;
-    kakao: string;
-    text_light1: string;
-    text_light2: string;
-    text_dark1: string;
-    text_dark2: string;
-  }
+  export type DefaultTheme = {
+    light: {
+      textColor: string;
+      bgColor: string;
+      primaryColor: string;
+      secondaryColor: string;
+      buttonText: string;
+      destructiveColor: string;
+      border: string;
+      gray: string;
+      gray_normal: string;
+      gray_lighter: string;
+      kakao: string;
+      text_light1: string;
+      text_light2: string;
+      text_dark1: string;
+      text_dark2: string;
+    },
+    dark : {
+      textColor: string;
+      bgColor: string;
+      primaryColor: string;
+      secondaryColor: string;
+      buttonText: string;
+      destructiveColor: string;
+      border: string;
+      gray: string;
+      gray_normal: string;
+      gray_lighter: string;
+      kakao: string;
+      text_light1: string;
+      text_light2: string;
+      text_dark1: string;
+      text_dark2: string;
+    }
+  };
 }
