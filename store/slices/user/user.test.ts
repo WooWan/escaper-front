dda7da3d3edb26437f217851b5c54fd62d876498
wooho <woohobi@gmail.com>
2022-10-25 15:39:57 +0900

    Test user 테스트 작성

---
+++ store/slices/user/user.test.ts
@@ -0,0 +1,35 @@
+import {store} from "../../config";
+import {IMember} from "../../../interfaces/member";
+import {loginUser, logoutUser, UserState} from "./user";
+
+describe("user test", () => {
+  let state:UserState;
+  let user: IMember;
+  beforeEach(() => {
+    state = store.getState().user;
+    user = {
+      id: 1,
+      username: "test",
+      profileImageUrl: "test",
+      providerType: "test",
+      roleType: "test",
+    };
+  })
+  it("initial state", () => {
+    expect(state.isLogin).toBe(false);
+  })
+
+  it("loginUser", () => {
+    store.dispatch(loginUser(user));
+    const newState = store.getState().user;
+    expect(newState.isLogin).toBe(true);
+    expect(newState.user).toEqual(user);
+  })
+
+  it("logout User", () => {
+    store.dispatch(logoutUser());
+    const newState = store.getState().user;
+    expect(newState.isLogin).toBe(false);
+    expect(newState.user).toBeNull();
+  })
+})
