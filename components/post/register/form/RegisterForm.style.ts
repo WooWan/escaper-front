979a5dccb3027ce0343e6001073fa4ba62ce214b
wooho <woohobi@gmail.com>
2022-10-27 16:56:54 +0900

    Fix remove text button

--- components/post/register/form/RegisterForm.style.ts
+++ components/post/register/form/RegisterForm.style.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import TextButton from "../../../core/button/text-button/TextButton";
 
 export const Container = styled.div`
   margin: 0 auto;
@@ -16,18 +15,13 @@ export const Form = styled.form`
   gap: 16px;
 `;
 
-export const SubmitButton = styled(TextButton).attrs({
-  type: "submit",
-  value: "글 등록",
-})``;
-
 export const Input = styled.input`
   width: 65%;
   padding: 0.5rem 0.4rem;
-  border: none;
+  border: none ${(props) => props.theme.gray_lighter};
   border-bottom: 2px solid;
-  border-color: ${(props) => props.theme.gray_lighter};
   font-size: 1rem;
+
   &:focus {
     outline: none;
   }
