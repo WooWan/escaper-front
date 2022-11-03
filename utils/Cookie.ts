98a5c5f5adab6fb4e4c656bfb126e3f49db4f22b
wooho <woohobi@gmail.com>
2022-11-03 22:17:36 +0900

    Feat remove cookie

--- utils/Cookie.ts
+++ utils/Cookie.ts
@@ -10,3 +10,7 @@ export const setCookie = (name: string, value: string | string[], option?: Cooki
 export const getCookie = (name: string): string => {
   return cookie.get(name)
 };
+
+export const removeCookie = (name: string) => {
+  return cookie.remove(name, {path: "/"});
+}
