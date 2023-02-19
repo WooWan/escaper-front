edb23bac89b0b8879743501ad33674daf9831a0f
WooWan <47740690+WooWan@users.noreply.github.com>
2023-02-19 21:41:15 +0900

    Add review querykey factory

---
+++ hooks/queries/review/queries.ts
@@ -0,0 +1,6 @@
+export const reviewKeys = {
+  all: ['reviews'] as const,
+  lists: () => [...reviewKeys.all, 'lists'] as const,
+  details: () => [...reviewKeys.all, 'details'] as const,
+  detail: (id: string) => [...reviewKeys.all, 'detail', id] as const,
+}
