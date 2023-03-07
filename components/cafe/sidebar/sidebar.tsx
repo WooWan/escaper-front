2d52f1a6bfad7352693a35cba8d7996186f75a2c
WooWan <woohobi@gmail.com>
2023-03-08 01:16:04 +0900

    Feat 카페 페이지 해쉬 라우팅

--- components/cafe/sidebar/sidebar.tsx
+++ components/cafe/sidebar/sidebar.tsx
@@ -1,8 +1,8 @@
-import { useRouter } from "next/router";
-import { useState } from "react";
-import styled from "styled-components";
-import { sidebarColumn, SidebarColumnTypes } from "../Cafe";
-import SidebarRow from "./sidebar-row";
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import styled from 'styled-components'
+import { sidebarColumn, SidebarColumnTypes } from '../Cafe'
+import SidebarRow from './sidebar-row'
 
 const Container = styled.div`
   display: flex;
@@ -11,40 +11,25 @@ const Container = styled.div`
   position: sticky;
   top: 250px;
   height: 100px;
-`;
+`
 
 function Sidebar() {
-  const [selected, setSelected] = useState("홈");
-  const router = useRouter();
-  const id = router.query.cafeId;
+  const [selected, setSelected] = useState('홈')
+  const router = useRouter()
+  const id = router.query.cafeId
   const handleHashRouting = (hashKey: SidebarColumnTypes) => {
     router.push({
       pathname: `/cafe/${id}`,
       hash: sidebarColumn[hashKey],
-    });
-  };
+    })
+  }
   return (
     <Container>
-      <SidebarRow
-        title={sidebarColumn["홈"]}
-        selected={selected}
-        setSelected={setSelected}
-        onClick={() => handleHashRouting(sidebarColumn["홈"])}
-      />
-      <SidebarRow
-        title={sidebarColumn["방탈출"]}
-        selected={selected}
-        setSelected={setSelected}
-        onClick={() => router.push("/cafe/5/#방탈출")}
-      />
-      <SidebarRow
-        title={sidebarColumn["리뷰"]}
-        selected={selected}
-        setSelected={setSelected}
-        onClick={() => handleHashRouting(sidebarColumn["리뷰"])}
-      />
+      <SidebarRow title={sidebarColumn['홈']} selected={selected} setSelected={setSelected} onClick={() => handleHashRouting(sidebarColumn['홈'])} />
+      <SidebarRow title={sidebarColumn['방탈출']} selected={selected} setSelected={setSelected} onClick={() => handleHashRouting('방탈출')} />
+      <SidebarRow title={sidebarColumn['리뷰']} selected={selected} setSelected={setSelected} onClick={() => handleHashRouting(sidebarColumn['리뷰'])} />
     </Container>
-  );
+  )
 }
 
-export default Sidebar;
+export default Sidebar
