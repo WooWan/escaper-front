8bb3ad2e839ec23f53cbf1a542e2fca6fa4f5775
WooWan <woohobi@gmail.com>
2023-03-08 01:11:59 +0900

    Update 별점 평가 시 테마 평점 업데이트

--- components/ratings/rating-section.tsx
+++ components/ratings/rating-section.tsx
@@ -1,27 +1,71 @@
-import AverageRating from "./average-rating/AverageRating";
-import UserRating from "./user-rating";
-import styled from "styled-components";
+import { Rating } from 'react-simple-star-rating'
+import { useCreateRating, useRating, useUpdateRating } from '@/hooks/queries/rating/useRating'
+import { useSession } from 'next-auth/react'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '@/tailwind.config.js'
 
-const Container = styled.ul`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  margin-top: 1rem;
-  max-height: 140px;
-  min-height: 100px;
-`;
-interface IProps {
-  memberRating: number | undefined;
-  count: number;
-  averageRating: number;
+resolveConfig
+type Props = {
+  id?: string
+  averageRating?: number
+  count: number
+  ratingCounts: number
 }
-function RatingSection({ memberRating, count, averageRating }: IProps) {
+
+function RatingSection({ id, count, averageRating, ratingCounts }: Props) {
+  const { data: session } = useSession()
+  const userId = session?.user.id
+  const { mutate: createRating } = useCreateRating()
+  const { mutate: updateRating } = useUpdateRating()
+  const { data } = useRating(String(userId), String(id))
+  const memberRating = data?.rating
+  const twFullConfig = resolveConfig(tailwindConfig)
+  const MAIN500 = twFullConfig?.theme?.colors?.main500 as string
+
+  const handleRate = (rating: number) => {
+    if (data) {
+      updateRating({
+        escapeThemeId: String(id),
+        userId: String(userId),
+        rating,
+      })
+    } else {
+      createRating({
+        escapeThemeId: String(id),
+        userId: String(userId),
+        rating,
+      })
+    }
+  }
+
   return (
-    <Container>
-      <UserRating rating={memberRating} />
-      <AverageRating rating={averageRating} count={count} />
-    </Container>
-  );
+    <div className="flex gap-4  sm:gap-20">
+      <div className="relative inline-flex h-32 flex-col items-center">
+        <h4 className="self-start text-b2">나의 별점</h4>
+        <span className="text-center text-h2">{memberRating ? memberRating : 0.0}</span>
+        <Rating
+          SVGstyle={{ display: 'inline-block' }}
+          fillColor={MAIN500}
+          SVGclassName="w-7 h-7 sm:w-9 sm:h-9"
+          allowFraction
+          onClick={handleRate}
+          initialValue={memberRating}
+        />
+      </div>
+      <div className="relative inline-flex h-32 flex-col items-center">
+        <h4 className="self-start text-b2">평균 별점</h4>
+        <span className="text-center text-h2">{averageRating}</span>
+        <Rating
+          SVGstyle={{ display: 'inline-block' }}
+          fillColor={MAIN500}
+          SVGclassName="w-7 h-7 sm:w-9 sm:h-9"
+          allowFraction
+          initialValue={averageRating}
+          readonly
+        />
+      </div>
+    </div>
+  )
 }
 
-export default RatingSection;
+export default RatingSection
---
+++ pages/api/rating/index.ts
@@ -0,0 +1,86 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET': {
+      return res.json(await getReview(req.query))
+    }
+    case 'POST': {
+      const result = await createRating(req.body)
+      await updateThemeRate(req.body.themeId)
+      return res.json(result)
+    }
+    case 'PUT': {
+      const result = await updateRating(req.body)
+      await updateThemeRate(req.body.themeId)
+      return res.json(result)
+    }
+  }
+}
+
+const getReview = async (
+  param: Partial<{
+    [key: string]: string | string[]
+  }>
+) => {
+  const { memberId, themeId } = param
+
+  const result = await prisma.review.findUnique({
+    where: {
+      escapeThemeId_userId: {
+        escapeThemeId: String(themeId),
+        userId: String(memberId),
+      },
+    },
+  })
+  return result
+}
+
+const createRating = async (body: any) => {
+  const { memberId, themeId, rating } = body
+  const result = await prisma.review.create({
+    data: {
+      userId: String(memberId),
+      escapeThemeId: String(themeId),
+      rating: rating,
+    },
+  })
+
+  return result
+}
+
+const updateRating = async (body: any) => {
+  const { memberId, themeId, rating } = body
+  const result = await prisma.review.update({
+    where: {
+      escapeThemeId_userId: {
+        userId: String(memberId),
+        escapeThemeId: String(themeId),
+      },
+    },
+    data: {
+      rating: rating,
+    },
+  })
+  return result
+}
+
+const updateThemeRate = async (id: string) => {
+  const reviews = await prisma.review.findMany({
+    where: {
+      escapeThemeId: id,
+    },
+  })
+
+  const rating = reviews.filter((review) => review.rating !== null).reduce((acc, cur) => acc + cur.rating!, 0) / reviews.length
+  const result = await prisma.escapeTheme.update({
+    where: {
+      id: id,
+    },
+    data: {
+      rating: rating,
+    },
+  })
+  return result
+}
