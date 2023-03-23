3f6127784789c33eab590fa754c530cb50bd8999
WooWan <woohobi@gmail.com>
2023-03-23 12:31:18 +0900

    Add 방탈출 테마 key factory

--- hooks/queries/escapeTheme/queries.ts
+++ hooks/queries/escapeTheme/queries.ts
@@ -1,5 +1,5 @@
 export const escapeThemeKey = {
-    all: ['escapeTheme'] as const,
-    details: () => [...escapeThemeKey.all, 'details'] as const,
-    detail: (id: string) => [...escapeThemeKey.all, 'detail', id] as const,
-  }
+  all: ['escapeTheme'] as const,
+  details: () => [...escapeThemeKey.all, 'details'] as const,
+  detail: (id: string) => [...escapeThemeKey.all, 'detail', id] as const,
+}
