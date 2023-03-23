e150bffe0d5cd862dfd383a1a5e4445900330565
WooWan <woohobi@gmail.com>
2023-03-23 12:31:28 +0900

    Feat 방탈출 검색 api

--- api/themes.ts
+++ api/themes.ts
@@ -5,3 +5,8 @@ export const getThemeListApi = async (): Promise<ThemeDetail[]> => {
   const response = await httpClient.get('/themes')
   return response.data
 }
+
+export const getThemeListByParamApi = async (param: string): Promise<ThemeDetail[]> => {
+  const response = await httpClient.get(`/themes/search?search=${param}`)
+  return response.data
+}
