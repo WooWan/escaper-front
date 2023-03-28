0a3a4fef572830ec0955a90a5fc076a7aa7b1b3e
WooWan <woohobi@gmail.com>
2023-03-28 23:33:25 +0900

    Update member type

--- types/member.d.ts
+++ types/member.d.ts
@@ -1,14 +1,22 @@
 export interface IMember {
-  profileImageUrl: string;
-  providerType: string;
-  roleType: string;
-  id: number;
-  username: string;
+  profileImageUrl: string
+  providerType: string
+  roleType: string
+  id: number
+  username: string
 }
 export interface ISignUpForm {
-  nickname: string;
+  nickname: string
 }
 
 export interface ISignUpRequest extends ISignUpForm {
-  email: string | string[] | undefined;
+  email: string | string[] | undefined
+}
+
+type User = {
+  email: string
+  emailVerified: boolean
+  id: string
+  image: string
+  name: string
 }
