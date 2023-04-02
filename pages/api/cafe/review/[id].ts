29de56c2bd98f0a7f31c651cffad86af889cb4df
WooWan <woohobi@gmail.com>
2023-04-02 23:58:44 +0900

    Feat fetch 리뷰 리스트 by user

---
+++ pages/api/cafe/review/[id].ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const id = req.query.id
+
+  const reviewList = await prisma.cafe.findUnique({
+    where: {
+      id: String(id),
+    },
+    include: {
+      themeList: {
+        include: {
+          reviewList: {
+            include: {
+              user: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  return res.status(200).json(reviewList)
+}
