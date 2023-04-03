c183c0897b84621d7c2a96929ed0de0a49f0dc41
WooWan <woohobi@gmail.com>
2023-04-04 02:07:37 +0900

    Fix design 별점 크기 버그

--- components/theme/theme-box/ThemeBox.tsx
+++ components/theme/theme-box/ThemeBox.tsx
@@ -5,20 +5,20 @@ import { Rating } from '@/components/ui/rating/Rating'
 
 function ThemeBox(theme: ThemeDetail) {
   return (
-    <div className="flex-shrink-0 grow-0 basis-[20%]">
+    <div>
       <Link key={theme.id} href={`/theme/${theme.id}`}>
         <div className="relative aspect-square">
           <Image src={theme.imageURL} fill alt="logo" />
         </div>
-        <div className="flex flex-col px-2">
-          <h4>{theme.name}</h4>
-          <div className="relative flex h-4 w-4 items-center">
-            <div className="flex items-baseline gap-2">
+        <div className="flex flex-col">
+          <span>{theme.name}</span>
+          <div className="relative flex h-4 w-0 items-center">
+            <div className="flex items-baseline gap-y-2">
               <Rating initialValue={theme.rating} readonly size={16} SVGstyle={{ display: 'inline-flex' }} />
               <span className="text-b1">{theme.rating}</span>
             </div>
           </div>
-          <div className="text-b2 text-gray-400">{theme.cafe.name}</div>
+          <div className="text-b2 text-gray-400">{theme.cafe?.name}</div>
         </div>
       </Link>
     </div>
