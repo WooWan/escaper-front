7e46207a86a28bcb9b5bfbf6fbe2c11b271189a1
WooWan <woohobi@gmail.com>
2023-04-04 17:38:19 +0900

    Update 헤더에 드랍다운 프로필 추가

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -2,10 +2,20 @@ import SearchBar from '@/components/layout/header/SearchBar'
 import Link from 'next/link'
 import React from 'react'
 import { User } from 'lucide-react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown/DropdownMenu'
+import { useRouter } from 'next/router'
 
 const Header = () => {
+  const router = useRouter()
   return (
-    <header className="flex max-h-[100px] items-center justify-between py-4 px-8">
+    <header className="flex max-h-[100px] items-center justify-between px-8 py-4">
       <Link href="/home">
         <div>
           <span className="text-h2 text-main500">Escapers</span>
@@ -15,7 +25,17 @@ const Header = () => {
       <div className="flex items-center gap-4">
         <SearchBar />
         <div className="cursor-pointer">
-          <User />
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <User />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => router.push('/inventory')}>방탈출 리뷰</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push('/mypage')}>마이 페이지</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </header>
