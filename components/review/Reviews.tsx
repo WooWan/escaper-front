bc9ec6c9da709e12968cbfed4dce6ec5295aab8f
WooWan <woohobi@gmail.com>
2023-04-08 23:57:07 +0900

    Update 리뷰 수정

--- components/review/Reviews.tsx
+++ components/review/Reviews.tsx
@@ -1,16 +1,17 @@
-import { ReviewType } from '@/types'
 import Review from '@/components/review/review/Review'
 import { ReviewResponse } from '@/types/review'
 
 type Props = {
+  openReviewEdit: () => void
   reviews?: ReviewResponse[]
   userReview?: ReviewResponse
+  userId?: string
 }
 
-function Reviews({ reviews, userReview }: Props) {
+function Reviews({ reviews, userReview, userId, openReviewEdit }: Props) {
   return (
     <ul className="mt-4 flex w-full flex-col border-t-[1px]">
-      <Review review={userReview} />
+      <Review review={userReview} userId={userId} openReviewEdit={openReviewEdit} />
       {reviews?.map((review) => {
         return <Review key={review.id} review={review} />
       })}
