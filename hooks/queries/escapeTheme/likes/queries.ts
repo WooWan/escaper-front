7c9247d6b77bd43f3092ba2df4ca97bc9ff26351
WooWan <woohobi@gmail.com>
2023-04-08 23:56:25 +0900

    Update 방탈출 테마 좋아요 keys user 추가

--- hooks/queries/escapeTheme/likes/queries.ts
+++ hooks/queries/escapeTheme/likes/queries.ts
@@ -2,4 +2,5 @@ export const escapeThemeLikeKeys = {
   all: ['escapeThemeLike'] as const,
   escapeThemes: () => [...escapeThemeLikeKeys.all, 'details'] as const,
   escapeTheme: (themeId: string) => [...escapeThemeLikeKeys.escapeThemes(), themeId] as const,
+  user: (userId: string) => [...escapeThemeLikeKeys.all, 'user', userId] as const,
 }
