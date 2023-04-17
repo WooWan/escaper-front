fabd4c221e2d1f66473707e2264a9177f5e4fd2e
WooWan <woohobi@gmail.com>
2023-04-17 15:11:32 +0900

    Remove unused css in js

--- components/review/review/Review.style.ts
+++ components/review/review/Review.style.ts
@@ -1,22 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const Container = styled.li`
-  padding: 1rem 0.5rem;
-  border-bottom: 1px solid rgb(238, 238, 238);
-`;
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
-`;
-export const Article = styled.article`
-  padding: 1.5rem 0;
-`;
-export const Footer = styled.footer`
-  display: flex;
-  align-items: center;
-`;
-export const ThumbUpWrapper = styled.div`
-  display: flex;
-  column-gap: 0.125rem;
-`;
+`
