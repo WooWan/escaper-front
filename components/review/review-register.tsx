3d62d03910af9a2183b20b5e0e65bd6de631561c
WooWan <woohobi@gmail.com>
2023-04-17 15:12:02 +0900

    Feat 리뷰 등록

--- components/review/review-register.tsx
+++ components/review/review-register.tsx
@@ -1,33 +1,26 @@
-import styled from 'styled-components'
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useDispatch } from 'react-redux'
 import useValidateUser from '@/utils/useValidateUser'
 import { openModal } from '@/store/slices/Modal'
-import { useAddReview } from '@/hooks/queries/review/useThemeReview'
-import Button from '../ui/button/Button'
+import { useUpsertReview } from '@/hooks/queries/review/useThemeReview'
 import { useSession } from 'next-auth/react'
+import { ReviewResponse } from '@/types/review'
+import { Button } from '@/components/ui/button/Button'
 
-const CommentTextArea = styled.textarea`
-  padding: 1rem 1rem 1.5rem;
-  width: 100%;
-  margin-bottom: 1.25rem;
-`
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
-function ReviewRegister() {
+type Props = {
+  userReview?: ReviewResponse
+}
+
+function ReviewRegister({ userReview }: Props) {
   const router = useRouter()
   const dispatch = useDispatch()
   const { validateUser } = useValidateUser()
   const themeId = router.query.id
   const { data: session } = useSession()
   const userId = session?.user.id
-  // const { user, isLogin } = useSelector(selectUser)
-
-  const [review, setReview] = useState('')
-  const { mutate: addReview } = useAddReview(String(themeId))
+  const [review, setReview] = useState(userReview?.review)
+  const { mutate: updateReview } = useUpsertReview()
 
   const onReviewHandle = () => {
     if (!session) {
@@ -38,36 +31,40 @@ function ReviewRegister() {
         })
       )
     } else {
-      addReview({
+      const updatedReview = {
+        ...userReview,
         escapeThemeId: String(themeId),
         userId: String(userId),
         review: review,
-      })
-      setReview('')
+      }
+      updateReview(updatedReview)
     }
   }
-
   const handleReviewChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setReview(event.target.value)
   }
-  return (
-    <>
-      <CommentTextArea
-        className="h-auto rounded-md border-2"
-        name=""
-        value={review}
-        id=""
-        placeholder="방탈출에 대한 내 평가를 남겨보세요!"
-        onChange={handleReviewChange}
-        onClick={validateUser}
-      />
-      <ButtonWrapper>
-        <Button intent="primary" size="small" onClick={onReviewHandle}>
-          리뷰 작성
-        </Button>
-      </ButtonWrapper>
-    </>
-  )
+
+  if (!userReview) {
+    return (
+      <>
+        <textarea
+          className="mb-1 h-auto w-full rounded-md border-2 p-3"
+          name=""
+          value={review}
+          id=""
+          placeholder="방탈출에 대한 내 평가를 남겨보세요!"
+          onChange={handleReviewChange}
+          onClick={validateUser}
+        />
+        <div className="flex justify-end">
+          <Button variant="primary" size="sm" onClick={onReviewHandle}>
+            리뷰 작성
+          </Button>
+        </div>
+      </>
+    )
+  }
+  return <></>
 }
 
 export default ReviewRegister
