be7ca1d8c039d4c38d679d9f4dc42c4e713127cc
WooWan <woohobi@gmail.com>
2023-04-17 20:36:18 +0900

    Feat 리뷰 삭제, 수정

--- pages/api/review/[id].ts
+++ pages/api/review/[id].ts
@@ -2,16 +2,42 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '@/lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const id = req.query.id?.toString()
+  switch (req.method) {
+    case 'GET': {
+      const id = req.query.id?.toString()
 
-  const reviewList = await prisma.review.findMany({
-    where: {
-      escapeThemeId: id,
-    },
-    include: {
-      user: true,
-    },
-  })
-
-  return res.status(200).json(reviewList)
+      const reviewList = await prisma.review.findMany({
+        where: {
+          escapeThemeId: id,
+        },
+        include: {
+          user: true,
+        },
+      })
+      return res.status(200).json(reviewList)
+    }
+    case 'DELETE': {
+      const id = req.query.id?.toString()
+      const review = await prisma.review.delete({
+        where: {
+          id: id,
+        },
+      })
+      return res.status(200).json(review)
+    }
+    case 'PUT': {
+      const id = req.query.id?.toString()
+      const { review, rating } = req.body
+      const result = await prisma.review.update({
+        where: {
+          id: id,
+        },
+        data: {
+          review: review,
+          rating: rating,
+        },
+      })
+      return res.status(200).json(result)
+    }
+  }
 }
