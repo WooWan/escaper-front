4970a0a5492535f274afcfed327f0f7360b3c88b
WooWan <woohobi@gmail.com>
2023-04-17 14:23:42 +0900

    Feat 방탈출 테마 좋아요 GET

---
+++ pages/api/theme/like/user.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const id = req.query.userId
+  switch (req.method) {
+    case 'GET': {
+      const themeList = await prisma.user.findUnique({
+        where: {
+          id: String(id),
+        },
+        select: {
+          likedEscapeThemes: {
+            select: {
+              themeId: true,
+              theme: true,
+            },
+          },
+        },
+      })
+      res.json(themeList?.likedEscapeThemes)
+      break
+    }
+  }
+}
