22245e322638b2a3742f70366c50c2080d0de76e
WooWan <woohobi@gmail.com>
2023-04-17 14:45:12 +0900

    Update next-auth user type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -5,10 +5,10 @@ declare module 'next-auth' {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-
     // custom user type
     user: {
       id: string
+      name: string
     }
   }
 }
